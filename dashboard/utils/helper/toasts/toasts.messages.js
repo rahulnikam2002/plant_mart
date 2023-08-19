5b0ff138efea17012ab0fb03ea50d610f3d4a7cd
Rahul Nikam <codewithrahulnikam@gmail.com>
2023-08-19 13:07:07 +0530

    feat: create helper functions for toast notifications and update README file

---
+++ dashboard/utils/helper/toasts/toasts.messages.js
@@ -0,0 +1,34 @@
+import { toast } from 'react-toastify';
+
+export const successToast = (msg) => toast.success(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+});
+
+export const errorToast = (msg) => toast.error(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+});
+
+export const warningToast = (msg) => toast.warning(msg, {
+    position: "top-right",
+    autoClose: 5000,
+    hideProgressBar: false,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined,
+    theme: "light",
+});
