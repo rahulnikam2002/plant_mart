4ae5fbccfa33445b3a5f05819dcc4518f8d1b1d6
Rahul Nikam <codewithrahulnikam@gmail.com>
2023-08-28 21:12:00 +0530

    Feat: install apex charts

--- dashboard/components/Layout/Layout.js
+++ dashboard/components/Layout/Layout.js
@@ -37,7 +37,7 @@ const Layout = ({ children }) => {
             )}
           </p>
         )}
-        {children}
+        <div style={{height: "100vh", overflow: "auto"}}>{children}</div>
       </div>
     </div>
   );
