b86c18109d9d957f0951f2f6257b1bbb679de10b
Siddhi <siddhimahadik257@gmail.com>
2023-08-29 14:00:49 +0530

    Feat: create header for add product page

--- dashboard/pages/products/new/index.js
+++ dashboard/pages/products/new/index.js
@@ -12,7 +12,16 @@ const NewProductPage = ()=>{
                 <div className={styles.headerGrp}>
                     <IconButton
                     bgColor={"#f5f7f9"}
-                     leftIcon={<i class="fi fi-rr-arrow-small-left"></i>} width={"fit-content"} />
+                    padding={"0px 10px"}
+                     leftIcon={<i style={{color:"#000"}} class="fi fi-rr-arrow-small-left"></i>} width={"fit-content"} />
+                     <div className={styles.textGrp}>
+                        <div className={styles.topTxt}>
+                            <p>Back to product list</p>
+                        </div>
+                        <div className={styles.bottomTxt}>
+                           <p>Add New Product</p>
+                        </div>
+                     </div>
                 </div>
             </div>
         </div>
