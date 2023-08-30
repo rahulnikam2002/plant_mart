2f251e829c08f6f3d69ae508800a7d0fbe7947ee
Siddhi <siddhimahadik257@gmail.com>
2023-08-30 15:01:17 +0530

    Feat: create product caregory and Inventory section on add new product

--- dashboard/pages/products/new/index.js
+++ dashboard/pages/products/new/index.js
@@ -1,7 +1,13 @@
 import { TextArea, TextInput } from '@/components/Inputs/Inputs';
 import { IconButton, LinkButton } from '@/components/buttons/buttons';
 import styles from '@/styles/newproduct.module.css'
+import { useState } from 'react';
 const NewProductPage = () => {
+    const [productname,setProductName]=useState("")
+    const [description,setDescription]=useState("")
+    const [prodCat,setProdCat]=useState("")
+    console.log(productname,description,prodCat)
+   
     return (
         <div className={styles.main}>
             <div className={styles.header}>
@@ -30,14 +36,38 @@ const NewProductPage = () => {
 
             <div className={styles.addProduct}>
                 <div className={styles.col1}>
-                    <p>Description</p>
-                    <div className={styles.prodDes}>
-                        <div className={styles.innerProdNameInput}>
-                            <TextInput label={"Product name"} placeholder={"Product name"} />
+                    <div>
+                        <p>Description</p>
+                        <div className={styles.prodDes}>
+                            <div className={styles.innerProdNameInput}>
+                                <TextInput setText={(e)=>setProductName(e.target.value)} label={"Product name"} placeholder={"Product name"} />
+                            </div>
+                            <div className={styles.innerProdDescInput}>
+                                <TextArea setText={(e)=>setDescription(e.target.value)} label={"Description"} placeholder={"Description"} />
+                            </div>
                         </div>
-
-                        <div className={styles.innerProdDescInput}>
-                            <TextArea label={"Description"} placeholder={"Description"} />
+                    </div>
+                    <div className={styles.prodCatArea}>
+                        <p>Category</p>
+                        <div className={styles.prodCat}>
+                            <div className={styles.innerProdInput}>
+                                <TextInput setText={(e)=>setProdCat(e.target.value)} label={"Product Category"} placeholder={"Health and Medicine"} />
+                            </div>
+                            <div className={styles.innerProdInput}>
+                                <TextInput label={"Product Category"} placeholder={"Beauty"} />
+                            </div>
+                            
+                        </div>
+                    </div>
+                    <div className={styles.inventory}>
+                        <p>Inventory</p>
+                        <div className={styles.inventoryinfo}>
+                            <div className={styles.quantity}>
+                                <TextInput label={"Quantity"} placeholder={1020}/>
+                            </div>
+                            <div className={styles.sku}>
+                                <TextInput label={"SKU"} placeholder={"uu-br-br-05"}/>
+                            </div>
                         </div>
                     </div>
                 </div>
