c435e34bc28c9f276b961875a12d7da3431086b2
Siddhi <siddhimahadik257@gmail.com>
2023-09-06 11:21:16 +0530

    Feat: create header and page title UI for all products page

--- dashboard/pages/products/index.js
+++ dashboard/pages/products/index.js
@@ -1,6 +1,8 @@
 import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
+import styles from '@/styles/allProducts.module.css'
 import { adminVerification } from "@/utils/helper/authentication/admin/admin.verification";
+import { Button, IconButton } from "@/components/buttons/buttons";
 const Chart = dynamic(() => import("react-apexcharts"), {
   ssr: false
 });
@@ -9,18 +11,47 @@ const Chart = dynamic(() => import("react-apexcharts"), {
 const ProductsPage = () => {
 
   return (
-    <div>
-      <p>Hello products!</p>
-<<<<<<< HEAD
-      {/* <Chart
-        options={options}
-        series={series}
-        type="area"
-        width="90%"
-      /> */}
-=======
-      
->>>>>>> a82c114b311899439b38b13d9df4980c99881c43
+    <div className={styles.main}>
+      <div className={styles.pageTitle}>
+        <div className={styles.headerGrp}>
+          <IconButton
+            bgColor={"#f5f7f9"}
+            padding={"0px 10px"}
+            leftIcon={
+              <i
+                style={{ color: "#000" }}
+                class="fi fi-rr-arrow-small-left"></i>
+            }
+            width={"fit-content"}
+          />
+          <div className={styles.textGrp}>
+            <div className={styles.topTxt}>
+              <p>Back to dashboard</p>
+            </div>
+            <div className={styles.bottomTxt}>
+              <p>All Products</p>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.productCta}>
+        <div className={styles.filter}>
+          <div className={styles.filterBy}>
+            <p>Filter By</p>
+          </div>
+          <div className={styles.prodFilter}>
+            <IconButton width={"fit-content"} rightIcon={<i style={{ color: "var(--primary)" }} class="fi fi-ss-angle-small-down"></i>} sx={{ color: "var(--primary)" }} bgColor={"#f5f7f9"} title={"All products"} />
+          </div>
+          <div className={styles.prodType}>
+            <IconButton width={"fit-content"} rightIcon={<i style={{ color: "var(--primary)" }} class="fi fi-ss-angle-small-down"></i>} sx={{ color: "var(--primary)" }} bgColor={"#f5f7f9"} title={"All type"} />
+
+          </div>
+        </div>
+        <div className={styles.addProduct}>
+        <IconButton leftIcon={<i style={{color:"var(--white)"}} class="fi fi-rr-add"></i>} title={"Add Product"} width={"fit-content"} padding={"10px"}/>
+        </div>
+      </div>
     </div>
   );
 };
@@ -39,7 +70,7 @@ export const getServerSideProps = async (ctx) => {
         }
       };
     }
-    return{
+    return {
       props: {
         isLogin: true
       }
