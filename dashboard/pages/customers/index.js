6d29c9c5679d717f1a073f0ac37b68887630bc73
Siddhi <siddhimahadik257@gmail.com>
2023-10-27 11:58:30 +0530

    Feat: UI for customer

--- dashboard/pages/customers/index.js
+++ dashboard/pages/customers/index.js
@@ -1,3 +1,4 @@
+import CustomerVerticalListing from '@/components/Listing/Product/Customer/vertical';
 import PageTitle from '@/components/pageTitle/title';
 import styles from '@/styles/allcustomers.module.css'
 
@@ -5,28 +6,52 @@ const AllCustomers = () => {
     return (
         <div className={styles.main}>
         <PageTitle title={"Customers"}/>
-            {/* <div className={styles.cutomer_header}>
+            <div className={styles.customer_header}>
                 <p>Name</p>
                 <p>Email</p>
                 <p>Phone number</p>
                 <p>Gender</p>
             </div>
-            <div className={styles.allCustomerlist}>
-                <div className={styles.name}>
-                    <img src="" alt="" />
-                    <p>John Doe</p>
-                </div>
-                <div className={styles.email}>
-                    <p>johndoe211@gmail.com</p>
-                </div>
-                <div className={styles.phnNumber}>
-                    <p>6968938555</p>
-                </div>
-                <div className={styles.gender}>
-                    <p>Male</p>
-                </div>
-            </div> */}
+            <div className={styles.allCutomerList}>
+            {dummyCustomers.map((value) => {
+          return (
+            <CustomerVerticalListing
+                cutomerName={value.name}
+                customerEmail={value.email}
+                customerNumber={value.phoneNumber}
+                customerGender={value.gender}
+            />)})}
+            </div>
+           
+            
         </div>
     )
 }
+
+
 export default AllCustomers;
+
+
+
+const dummyCustomers = [{
+    name: "John",
+    email:"john321@gmail.com",
+    phoneNumber:"8956688194",
+    gender:"Male"
+  
+  },
+  {
+    name: "Samiksha",
+    email:"samiksha234@gmail.com",
+    phoneNumber:"8956688194",
+    gender:"Female"
+  },
+  
+  {
+    name: "Neha",
+    email:"neha21@gmail.com",
+    phoneNumber:"9236688194",
+    gender:"Female"
+  }
+  ] 
+
